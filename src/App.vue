6ec7f159f881b61ebbeff204b2727ad958b96b79
MuhammadRazaAslam <raza@sendoso.com>
2022-07-25 12:30:14 +0500

    Initial Setup of Vue app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='grey-background'>
+    <Header />
+    <CulturalActivities msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/shared/AppHeader.vue'
+import CulturalActivities from './components/CulturalActivities.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    CulturalActivities
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.grey-background {
+  background-color: #f3f2f2 !important;
 }
 </style>
---
+++ src/components/shared/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header class='main-header'>
+      <div class='logo'>
+        <img alt="Cultural Activities Logo" src="@/assets/images/logo.png">
+      </div>
+    </header>
+    <main>
+    </main>
+  </div>
+</template>
+
+<style>
+.main-header {
+  background-color: #fff;
+  width: 90%;
+  margin-left: 5%;
+}
+.logo {
+  align-items: center !important;
+}
+.logo img {
+  width: 250px;
+}
+main {
+  min-height: 800px;
+}
+</style>
